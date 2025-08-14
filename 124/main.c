0375873c01859bc9f967852274cd43692271b926
supharoek kanket <674244124@mail.pbru.ac.th>
2025-08-14 11:41:13 +0700

    Add files via upload

---
+++ 124/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main() {
+	 printf("------------------------------\n");
+    printf("My bio.: \n");
+    printf("ID : 654244124 \n");
+    printf("Name : Supharoek Kanket\n");
+    printf("------------------------------\n");
+    getchar();
+}
+
